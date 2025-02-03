36f63c3bb6614e94de52c3b2503d1fe6e8e434a2
petehuu <petehuu@gmail.com>
2025-02-04 01:04:45 +0200

    Update express-server.js

--- .github/workflows/express-server.js
+++ .github/workflows/express-server.js
@@ -7,13 +7,13 @@ const port = 3000;
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, '..', '..')));
-
 // Lokitus saapuvista pyynnöistä
 app.use((req, res, next) => {
     console.log(`Received request: ${req.method} ${req.url}`);
     next();
 });
+
+app.use(express.static(path.join(__dirname, '..', '..')));
 
 // Proxy-middleware
 app.use('/proxy', createProxyMiddleware({
