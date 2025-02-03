b0a61498af6b75fbab6aa3faa21924d59be7b811
petehuu <petehuu@gmail.com>
2025-02-04 00:21:04 +0200

    Update express-server.js

--- .github/workflows/express-server.js
+++ .github/workflows/express-server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const app = express();
-const port = 8081; // Vaihdettu porttiin 8081
+const port = 8081;
 
 app.use(cors());
 
