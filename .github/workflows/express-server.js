2c1477c03d4196b71a71f94ce6b834b0ee01d0f1
petehuu <petehuu@gmail.com>
2025-02-04 01:01:10 +0200

    Update express-server.js

--- .github/workflows/express-server.js
+++ .github/workflows/express-server.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const port = 3000;
 
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, '..', '..')));
+
+// Lokitus saapuvista pyynnöistä
+app.use((req, res, next) => {
+    console.log(`Received request: ${req.method} ${req.url}`);
+    next();
+});
+
+// Proxy-middleware
+app.use('/proxy', createProxyMiddleware({
+  target: '', // Ei asetettu kohdetta, annetaan pyynnön määrittää kohde
+  changeOrigin: true,
+  pathRewrite: {
+    '^/proxy': '' // Poistetaan /proxy reitistä
+  },
+  router: (req) => {
+    const target = req.query.url; // Käytetään query-parametria kohteen määrittämiseen
+    console.log(`Proxying request to: ${target}`);
+    return target;
+  }
+}));
 
 app.get('/', (req, res) => {
     const indexPath = path.join(__dirname, '..', '..', 'index.html');
