5740c8b142e507ca0206f29edeebdec4952b157e
petehuu <petehuu@gmail.com>
2025-02-04 14:36:25 +0200

    Update express-server.js

--- .github/workflows/express-server.js
+++ .github/workflows/express-server.js
@@ -50,3 +50,9 @@ server.on('error', (err) => {
     console.error('Server error:', err);
 });
 
+app.use((err, req, res, next) => {
+    console.error(`Error handling request for ${req.method} ${req.url}:`, err);
+    res.status(500).send('Something went wrong!');
+});
+
+
