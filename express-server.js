fddafa373e01b0a61fc8d7d1f446a5b2e76fe89d
petehuu <petehuu@gmail.com>
2025-02-06 16:30:52 +0200

    Update express-server.js

--- express-server.js
+++ express-server.js
@@ -13,8 +13,8 @@ app.use(cors({
 app.use(express.static(path.join(__dirname)));
 
 app.use('/proxy', createProxyMiddleware({
-  target: 'http://www.google.com',
-  //target: 'https://hkp.maanmittauslaitos.fi/hkp/action?action_route=Coordinates&lat=59.98070939695573&lon=24.40447331965473&srs=EPSG:4326&targetSRS=EPSG:3067',
+  //target: 'http://www.google.com',
+  target: 'https://hkp.maanmittauslaitos.fi/hkp/action?action_route=Coordinates&lat=59.98070939695573&lon=24.40447331965473&srs=EPSG:4326&targetSRS=EPSG:3067',
   changeOrigin: true,
   pathRewrite: {
     '^/proxy': '',
